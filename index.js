a4f13b935742b2e4bb60c53264079b02e47f834b
Hazem El Sankari <hazemleb96@gmail.com>
2021-04-13 17:17:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,25 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString){
+  let th=timeString.split(":")[0];
+  
+  if (th<12){
+    return "Good Morning";
+  }
+  else if (th>12 && th<17){
+    return "Good Afternoon";
+  }
+  else{
+      return "Good Evening";
+  }
+}
 /* Write your implementation of displayMessage() */
+ let testContent = "TEST";
+  
+function displayMessage(testContent){
+  let content = document.getElementById("greeting").innerText;
+  content == testContent;
+}
+
+displayMessage(testContent);
